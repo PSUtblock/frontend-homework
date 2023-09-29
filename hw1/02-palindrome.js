9d27b4c8a15ca0f12fd672168393822f276fc3e3
Travis Block <tblock@pdx.edu>
2023-09-29 01:48:19 -0700

    finished function and some bootstrap

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,21 @@
 const elem = document.querySelector('input');
+const response = document.getElementById('response');
 
 elem.addEventListener('input', handleInput);
+
+function handleInput() {
+    
+    let fwd = elem.value.toString();
+    let bwd = fwd.split("").reverse().join("");
+
+    (fwd == null || fwd == "") ? response.hidden = true : response.hidden = false;
+
+    if(fwd == bwd) {
+        response.innerText = `${fwd} is a palindrome!`;
+        response.classList = "alert alert-primary";
+    }
+    else {
+        response.innerText = `${fwd} is not a palindrome!`;
+        response.classList = "alert alert-danger";
+    }
+}
