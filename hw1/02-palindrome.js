742e2edd5b2e2dec222e3c18802f28bab2895077
Travis Block <tblock@pdx.edu>
2023-10-03 11:39:40 -0700

    satisfied with code

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,16 +1,22 @@
 const elem = document.querySelector('input');
 const response = document.getElementById('response');
 
-elem.addEventListener('input', handleInput);
+//function that checks if the number entered is a palindrome
+const handleInput = function handleInput() {
 
-function handleInput() {
-    
+    //quick way to not allow for negative integers or decimal values
+    if(elem.value)
+        elem.value = Math.floor(Math.abs(elem.value));
+
+    //converting entry to string and making a copy to reverse and compare the values.
     let fwd = elem.value.toString();
     let bwd = fwd.split("").reverse().join("");
 
-    (fwd == null || fwd == "") ? response.hidden = true : response.hidden = false;
+    // If the entered value is empty hide the response div.
+    (fwd === "") ? response.hidden = true : response.hidden = false;
 
-    if(fwd == bwd) {
+    // Cases to fill the response div when the user starts to enter numbers
+    if(fwd === bwd) {
         response.innerText = `${fwd} is a palindrome!`;
         response.classList = "alert alert-primary";
     }
@@ -18,4 +24,7 @@ function handleInput() {
         response.innerText = `${fwd} is not a palindrome!`;
         response.classList = "alert alert-danger";
     }
-}
+};
+
+// Event Listener to call handleInput
+elem.addEventListener('input', handleInput);
